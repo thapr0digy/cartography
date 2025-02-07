4b27b88b39209be7faa58603efd75dc59fc5672f
Alex Chantavy <alex@subimage.io>
2025-02-07 09:54:56 -0800

    Add integration test for aws.ecr.sync() (#1447)
    ### Summary
    > Describe your changes.
    
    Improves test coverage of aws.ecr as we're prepping for bigger
    refactors. This PR has no functional changes.
    
    
    
    ### Checklist
    
    Provide proof that this works (this makes reviews move faster). Please
    perform one or more of the following:
    - [x ] Update/add unit or integration tests.
    - [ ] Include a screenshot showing what the graph looked like before and
    after your changes.
    - [ ] Include console log trace showing what happened before and after
    your changes.
    
    If you are changing a node or relationship:
    - [ ] Update the
    [schema](https://github.com/lyft/cartography/tree/master/docs/root/modules)
    and
    [readme](https://github.com/lyft/cartography/blob/master/docs/schema/README.md).
    
    If you are implementing a new intel module:
    - [ ] Use the NodeSchema [data
    model](https://cartography-cncf.github.io/cartography/dev/writing-intel-modules.html#defining-a-node).
    
    ---------
    
    Signed-off-by: Alex Chantavy <chantavy@gmail.com>

--- tests/integration/cartography/intel/aws/test_ecr.py
+++ tests/integration/cartography/intel/aws/test_ecr.py
@@ -1,9 +1,13 @@
 import datetime
 import json
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
 import cartography.intel.aws.ecr
 import tests.data.aws.ecr
 from tests.integration.cartography.intel.aws.common import create_test_account
+from tests.integration.util import check_nodes
+from tests.integration.util import check_rels
 
 TEST_ACCOUNT_ID = '000000000000'
 TEST_REGION = 'us-east-1'
@@ -19,6 +23,158 @@ def _ensure_local_neo4j_has_test_ecr_repo_data(neo4j_session):
         TEST_ACCOUNT_ID,
         TEST_UPDATE_TAG,
     )
+
+
+@patch.object(
+    cartography.intel.aws.ecr,
+    'get_ecr_repositories',
+    return_value=tests.data.aws.ecr.DESCRIBE_REPOSITORIES['repositories'],
+)
+@patch.object(
+    cartography.intel.aws.ecr,
+    'get_ecr_repository_images',
+    side_effect=[
+        tests.data.aws.ecr.LIST_REPOSITORY_IMAGES['000000000000.dkr.ecr.us-east-1/example-repository'],
+        tests.data.aws.ecr.LIST_REPOSITORY_IMAGES['000000000000.dkr.ecr.us-east-1/sample-repository'],
+        tests.data.aws.ecr.LIST_REPOSITORY_IMAGES['000000000000.dkr.ecr.us-east-1/test-repository'],
+    ],
+)
+def test_sync_ecr(mock_get_images, mock_get_repos, neo4j_session):
+    """
+    Ensure that ECR repositories and images are properly synced and connected
+    """
+    # Arrange
+    boto3_session = MagicMock()
+    create_test_account(neo4j_session, TEST_ACCOUNT_ID, TEST_UPDATE_TAG)
+
+    # Act
+    cartography.intel.aws.ecr.sync(
+        neo4j_session,
+        boto3_session,
+        [TEST_REGION],
+        TEST_ACCOUNT_ID,
+        TEST_UPDATE_TAG,
+        {'UPDATE_TAG': TEST_UPDATE_TAG, 'AWS_ID': TEST_ACCOUNT_ID},
+    )
+
+    # Assert ECR repositories exist
+    assert check_nodes(neo4j_session, 'ECRRepository', ['id', 'name']) == {
+        ('arn:aws:ecr:us-east-1:000000000000:repository/example-repository', 'example-repository'),
+        ('arn:aws:ecr:us-east-1:000000000000:repository/sample-repository', 'sample-repository'),
+        ('arn:aws:ecr:us-east-1:000000000000:repository/test-repository', 'test-repository'),
+    }
+
+    # Assert ECR images exist (excluding those without digests)
+    assert check_nodes(neo4j_session, 'ECRImage', ['id', 'digest']) == {
+        (
+            'sha256:0000000000000000000000000000000000000000000000000000000000000000',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000000',
+        ),
+        (
+            'sha256:0000000000000000000000000000000000000000000000000000000000000001',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000001',
+        ),
+        (
+            'sha256:0000000000000000000000000000000000000000000000000000000000000011',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000011',
+        ),
+        (
+            'sha256:0000000000000000000000000000000000000000000000000000000000000021',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000021',
+        ),
+        (
+            'sha256:0000000000000000000000000000000000000000000000000000000000000031',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000031',
+        ),
+    }
+
+    # Assert ECR repository images exist
+    assert check_nodes(neo4j_session, 'ECRRepositoryImage', ['id', 'tag']) == {
+        ('000000000000.dkr.ecr.us-east-1/example-repository:1', '1'),
+        ('000000000000.dkr.ecr.us-east-1/example-repository:2', '2'),
+        ('000000000000.dkr.ecr.us-east-1/sample-repository:1', '1'),
+        ('000000000000.dkr.ecr.us-east-1/sample-repository:2', '2'),
+        ('000000000000.dkr.ecr.us-east-1/test-repository:1234567890', '1234567890'),
+        ('000000000000.dkr.ecr.us-east-1/test-repository:1', '1'),
+        ('000000000000.dkr.ecr.us-east-1/test-repository', None),
+    }
+
+    # Assert repository to AWS account relationship
+    assert check_rels(
+        neo4j_session,
+        'ECRRepository',
+        'id',
+        'AWSAccount',
+        'id',
+        'RESOURCE',
+        rel_direction_right=False,
+    ) == {
+        ('arn:aws:ecr:us-east-1:000000000000:repository/example-repository', '000000000000'),
+        ('arn:aws:ecr:us-east-1:000000000000:repository/sample-repository', '000000000000'),
+        ('arn:aws:ecr:us-east-1:000000000000:repository/test-repository', '000000000000'),
+    }
+
+    # Assert repository to repository image relationship
+    assert check_rels(
+        neo4j_session,
+        'ECRRepository',
+        'uri',
+        'ECRRepositoryImage',
+        'id',
+        'REPO_IMAGE',
+        rel_direction_right=True,
+    ) == {
+        ('000000000000.dkr.ecr.us-east-1/example-repository', '000000000000.dkr.ecr.us-east-1/example-repository:1'),
+        ('000000000000.dkr.ecr.us-east-1/example-repository', '000000000000.dkr.ecr.us-east-1/example-repository:2'),
+        ('000000000000.dkr.ecr.us-east-1/sample-repository', '000000000000.dkr.ecr.us-east-1/sample-repository:1'),
+        ('000000000000.dkr.ecr.us-east-1/sample-repository', '000000000000.dkr.ecr.us-east-1/sample-repository:2'),
+        ('000000000000.dkr.ecr.us-east-1/test-repository', '000000000000.dkr.ecr.us-east-1/test-repository:1234567890'),
+        ('000000000000.dkr.ecr.us-east-1/test-repository', '000000000000.dkr.ecr.us-east-1/test-repository:1'),
+        ('000000000000.dkr.ecr.us-east-1/test-repository', '000000000000.dkr.ecr.us-east-1/test-repository'),
+    }
+
+    # Assert repository image to image relationship
+    assert check_rels(
+        neo4j_session,
+        'ECRRepositoryImage',
+        'id',
+        'ECRImage',
+        'id',
+        'IMAGE',
+        rel_direction_right=True,
+    ) == {
+        (
+            '000000000000.dkr.ecr.us-east-1/example-repository:1',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000000',
+        ),
+        (
+            '000000000000.dkr.ecr.us-east-1/example-repository:2',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000001',
+        ),
+        (
+            '000000000000.dkr.ecr.us-east-1/sample-repository:1',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000000',
+        ),
+        (
+            '000000000000.dkr.ecr.us-east-1/sample-repository:2',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000011',
+        ),
+        (
+            '000000000000.dkr.ecr.us-east-1/test-repository:1234567890',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000000',
+        ),
+        (
+            '000000000000.dkr.ecr.us-east-1/test-repository:1',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000021',
+        ),
+        (
+            '000000000000.dkr.ecr.us-east-1/test-repository',
+            'sha256:0000000000000000000000000000000000000000000000000000000000000031',
+        ),
+    }
+
+    # Clean up the database after the test
+    neo4j_session.run("MATCH (n) detach delete n")
 
 
 def test_load_ecr_repositories(neo4j_session):
